bb1e6f73477aac2c1f3e510d1c65b8e459a4171e
Onur Ak <onurak84@gmail.com>
2013-03-21 22:17:21 +0200

    Initial Commit

---
+++ Tap Me/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tap Me
+//
+//  Created by Onur Ak on 3/21/13.
+//  Copyright (c) 2013 Onur Ak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Tap Me/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Tap Me
+//
+//  Created by Onur Ak on 3/21/13.
+//  Copyright (c) 2013 Onur Ak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
